cef3196451f419aa5b20c6666a0cea21b6787242
AnthonyTC89 <ptonyptc@gmail.com>
2020-01-13 16:26:51 -0500

    App component done

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
+import BooksForm from '../containers/BooksForm';
+import BooksList from '../containers/BooksList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BooksList />
+      <BooksForm />
     </div>
   );
 }
