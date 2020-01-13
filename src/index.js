20595c61e69e10547061544c45be545d612d619f
Tiago Ferreira (ferrreirati) <tiferreira12@gmail.com>
2020-01-13 20:01:38 -0300

    feat: add InitialState and Provider from ReactRedux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import uuidv4 from 'uuid/v4';
+import ReactRedux from 'react-redux';
 import './index.css';
 import App from './components/App';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = [
+  {
+    id: uuidv4(),
+    title: 'Game of Thrones',
+    category: 'Learning',
+  }, {
+    id: uuidv4(),
+    title: 'Ruby on Rails - The Complete Guide',
+    category: 'Learning',
+  }, {
+    id: uuidv4(),
+    title: 'Harry Potter',
+    category: 'kids',
+  },
+];
+
+const store = React.createStore(rootReducer, initialState);
+const { Provider } = ReactRedux;
+
+ReactDOM.render((
+  <Provider store={store}>
+    <App />
+  </Provider>
+), document.getElementById('root'));
