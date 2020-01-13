dfcdffd86e7825b54246de5a55c45f366e255990
AnthonyTC89 <ptonyptc@gmail.com>
2020-01-13 17:27:13 -0500

    BookForm component done

---
+++ src/containers/BooksForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const BookForm = () => {
+  const categories = ['Action', 'Biography', 'History', 'Horror', 
+    'Kids', 'Learning', 'Sci-Fi'];
+  return (
+    <form>
+      <input placeholder="Type the title" />
+      <select>
+        { categories.map((category) => (
+          <option value={category}>
+            { category }
+          </option>
+        ))}
+      </select>
+      <button type="submit">Add Book</button>
+    </form>
+  );
+};
+
+export default BookForm;
