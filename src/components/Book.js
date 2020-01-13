5e0c03bf4181885d17958efb2dd436e844b8a2ec
AnthonyTC89 <ptonyptc@gmail.com>
2020-01-13 17:12:51 -0500

    Book Component done

---
+++ src/components/Book.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Book = (props) => {
+  const { book } = props;
+  return (
+    <tr>
+      <td>
+        { book.id }
+      </td>
+      <td>
+        { book.title }
+      </td>
+      <td>
+        { book.category }
+      </td>
+    </tr>
+  );
+};
+
+Book.propTypes = {
+  book: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  }).isRequired,
+};
